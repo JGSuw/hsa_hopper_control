be2f18b459dc7880b8bf68943d9997af08ebb372
Joseph Sullivan <jgs6156@uw.edu>
2024-10-16 15:49:59 -0700

    updating hsa model, especially dissipation component

--- src/hsa_hopper/hsa_model.py
+++ src/hsa_hopper/hsa_model.py
@@ -4,12 +4,16 @@ import os
 
 class HSAPotential:
     def __init__(self, 
-                 v: np.ndarray, # kernel weights i.e. estimates of potential at configuration
-                 y: np.ndarray, # sample configurations corresponding to weights in v
-                 s: float):     # length-scale parameter used by kernels
+                 w_c: np.ndarray,   # conservative kernel weights i.e. estimates of potential at configuration
+                 w_d: np.ndarray,   # dissipative kernel weights i.e. estimates of potential at configuration
+                 y: np.ndarray,     # sample configurations corresponding to weights in v
+                 l: float,          # smoothness parameter for dissipation potential
+                 s: float):         # length-scale parameter used by kernels
         self.N = y.shape[0]
-        self.v = v
+        self.w_c = w_c
+        self.w_d = w_d
         self.y = y
+        self.l = l
         self.s = s
         self.S = S = np.cov(y.T)
         self.Sinv = Sinv = np.linalg.inv(S)
@@ -19,28 +23,45 @@ class HSAPotential:
         self.drho = lambda z, i: 2*Sinv@(z-y[i,:])/s**2
         self.d2rho = lambda z, i: 2*Sinv/s**2
 
-        # square exponential kernel, its gradient, and hessian
-        self.k = lambda z, i: np.exp(-self.rho(z,i))
-        self.dk = lambda z, i: (self.k(z,i))*(-self.drho(z,i))
-        self.d2k = lambda z, i: self.k(z,i)*(self.drho(z,i)@self.drho(z,i).T - self.d2rho(z,i))
+        # conservative kernel, its gradient, and hessian
+        self.kc = lambda z, i: np.exp(-self.rho(z,i))
+        self.dkc = lambda z, i: (self.kc(z,i))*(-self.drho(z,i))
+        # self.d2k = lambda z, i: self.k(z,i)*(self.drho(z,i)@self.drho(z,i).T - self.d2rho(z,i))
 
-    def V(self, z: np.array):
-        return sum(self.v[i]*self.k(z,i) for i in range(self.N))
+        # activation function and derivative, used in disspation kernel
+        self.f = lambda x: np.log((1+np.exp(self.l*x))*(1+np.exp(-self.l*x))/4)
+        self.df = lambda x: (4*self.l)*(np.exp(self.l*x)-1)/(np.exp(self.l*x)+1)
+
+        # disspation kernel and its gradient
+        self.kd = lambda z, zdot, i: self.f(self.drho(z,i)@zdot)*np.exp(-self.rho(z,i))
+        self.dkd = lambda z, zdot, i: self.df(self.drho(z,i)@zdot)*np.exp(-self.rho(z,i))*self.drho(z,i)
+
+
+    def V(self, z: np.ndarray):
+        return sum(self.w_c[i]*self.kc(z,i) for i in range(self.N))
+
+    def R(self, z: np.ndarray, zdot: np.ndarray):
+        return sum(self.w_d[i]*self.kd(z,zdot,i) for i in range(self.N))
     
-    def dV(self, z: np.array):
-        return sum(self.v[i]*self.dk(z,i) for i in range(self.N))
+    def dV(self, z: np.ndarray):
+        return sum(self.w_c[i]*self.dkc(z,i) for i in range(self.N))
 
-    def d2V(self, z: np.array):
-        return sum(self.v[i]*self.d2k(z,i) for i in range(self.N))
+    def dR(self, z: np.ndarray, zdot: np.ndarray):
+        return sum(self.w_d[i]*self.dkd(z,zdot,i) for i in range(self.N))
+
+    # def d2V(self, z: np.ndarray):
+    #     return sum(self.v[i]*self.d2k(z,i) for i in range(self.N))
     
     def attribute_dict(self):
-        attributes = {'v': self.v.tolist(), 'y': self.y.tolist(), 's': self.s}
+        attributes = {'w_c': self.w_c.tolist(), 'w_d': self.w_d.tolist(), 'y': self.y.tolist(), 'l': self.l, 's': self.s}
         return attributes
     
     def make_from_dict(attributes):
         return HSAPotential(
-            np.array(attributes['v']), 
+            np.array(attributes['w_c']), 
+            np.array(attributes['w_d']), 
             np.array(attributes['y']), 
+            attributes['l'],
             attributes['s'])
 
 def load_potential(path:os.path):
